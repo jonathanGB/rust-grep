5be129fd9135a2f20ff0723cf2ef1b5da0410118
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2019-06-30 17:11:02 -0400

    nit change to new config, skip the extra closure

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use minigrep::Config;
 use structopt::StructOpt;
 
 fn main() {
-    let config = Config::new(|| Config::from_args());
+    let config = Config::new(Config::from_args);
 
     if let Err(e) = minigrep::run(config) {
         eprintln!("Problem running the application\n{}", e);
